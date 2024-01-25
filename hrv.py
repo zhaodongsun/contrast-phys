b7d120170c547718090ec1e767c1841a852c9881
Zhaodong Sun <zhaodongsun@gmail.com>
2024-01-25 18:02:25 +0800

    Update hrv.py

--- hrv.py
+++ hrv.py
@@ -9,12 +9,7 @@ def get_ibi(sig, sig_fps):
     info = nk.ppg_findpeaks(ppg_clean,sampling_rate=sig_fps)
     peak_loc = info["PPG_Peaks"]
     
-    IPI = (peak_loc[1:]-peak_loc[0:-1])/sig_fps
-    T = np.zeros(len(IPI));
-    for i in range(len(IPI)):
-        T[i] = np.sum(IPI[:i])
-
-    IBI = np.array([T, IPI])
+    IBI = (peak_loc[1:]-peak_loc[0:-1])/sig_fps
     return IBI
 
 def freq_features(ibi):
@@ -26,13 +21,13 @@ def freq_features(ibi):
         peak_loc = peak_loc[highest_peak_loc]
         return peak_loc
     
-    freq,power = LombScargle(np.cumsum(ibi)/1000, ibi).autopower(minimum_frequency=0.01,maximum_frequency=0.51)
+    freq,power = LombScargle(np.cumsum(ibi), ibi).autopower(minimum_frequency=0.01,maximum_frequency=0.51)
     idx_lf = np.where((freq>0.04)*(freq<0.15))
     idx_hf = np.where((freq>0.15)*(freq<0.40))
     
     freq_lf = freq[idx_lf]
     power_lf = np.sum(power[idx_lf])
-    peak_loc_lf= 0 #find_highest_peak(power[idx_lf])
+    peak_loc_lf= find_highest_peak(power[idx_lf])
     
     freq_hf = freq[idx_hf]
     power_hf = np.sum(power[idx_hf])
