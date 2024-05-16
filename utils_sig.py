eb33acf418f9bc57724313a7ae48f6cf1f2e16de
Zhaodong Sun <zhaodongsun@gmail.com>
2024-05-16 11:20:57 +0300

    add SNR metric in utils_sig.py

--- utils_sig.py
+++ utils_sig.py
@@ -110,3 +110,38 @@ def hr_fft_batch(sig_list, fs, harmonics_removal=True):
 
 def normalize(x):
     return (x-x.mean())/x.std()
+
+def SNR_get(waveform, gt_hr, fs, filtered=False):
+    waveform = np.reshape(waveform, -1)
+    if filtered:
+        waveform = butter_bandpass(waveform, 0.6, 4, 60)
+    N = waveform.shape[0]
+    
+#     low_idx = np.round(0.6 / fs * waveform.shape[0]).astype('int')
+#     high_idx = np.round(4 / fs * waveform.shape[0]).astype('int')
+    
+#     waveform[:low_idx] = 0
+#     waveform[high_idx:] = 0
+    
+    bin1 = round(5/60/fs*N)
+    bin2 = round(10/60/fs*N)
+
+    f1 = gt_hr / 60
+    f2 = f1 * 2
+
+    bc1 = round(f1*N/fs)
+    bc2 = round(f2*N/fs)
+
+    window = signal.windows.hann(N)
+    win_waveform = waveform * window
+    waveform_f = np.abs(fft(win_waveform))**2
+
+    total_power = np.sum(waveform_f)
+    signal_power1 = 2*np.sum(waveform_f[bc1-bin1:bc1+bin1])
+    signal_power2 = 2*np.sum(waveform_f[bc2-bin2:bc2+bin2])
+
+    signal_power = signal_power1 + signal_power2
+    noise_power = total_power - signal_power
+
+    snr = 10*np.log10(signal_power/noise_power)
+    return snr
